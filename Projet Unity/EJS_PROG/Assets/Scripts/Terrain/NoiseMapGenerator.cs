43ab4768fa9ec1cea2707d4a7cc1aaf6b512ce0f
Emmanuel Janssens <emmanueljanssens72@gmail.com>
2018-02-13 12:11:40 +0100

    Génération d'un terrain
    Génération d'un heighMap et d'un terrain

---
+++ Projet Unity/EJS_PROG/Assets/Scripts/Terrain/NoiseMapGenerator.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Used to generate terrain
+/// </summary>
+public  class NoiseMapGenerator : MonoBehaviour 
+{
+
+    public static NoiseMapGenerator current;
+
+    [SerializeField]
+	private int _MapHeight = 256;
+    [SerializeField]
+    private int _MapWidth = 256 ;
+    [SerializeField]
+    private float _NoiseScale = .4f;
+
+    private Texture2D _GeneratedTexture;
+    private Material _GeneratedMaterial;
+    private Color[] _PixelColor; // black/white
+
+    private float[,] _Noise;
+
+    public Renderer _NoiseMapRenderer;
+
+
+    public int Height { get { return _MapHeight; } }
+    public int Width { get { return _MapWidth;  } }
+    public float Scale { get { return _NoiseScale; } }
+
+	// Use this for initialization
+	void Start () 
+	{
+        if (current == null)
+            current = this;
+
+        _Noise = new float[_MapWidth, _MapHeight];
+        _PixelColor = new Color[_MapWidth * _MapHeight];
+        _GeneratedTexture = new Texture2D(_MapWidth, _MapHeight);
+	}
+	
+    public static NoiseMapGenerator Get()
+    {
+        return current;
+    }
+
+    public void Generate()
+    {
+        for(int y = 0; y < _MapHeight; y++)
+        {
+            for(int x = 0; x < _MapWidth; x++)
+            {
+                float xCoord, yCoord;
+                xCoord = x /  _NoiseScale;
+                yCoord = y / _NoiseScale;
+
+                float perlinValue = Mathf.PerlinNoise(xCoord, yCoord);
+
+                _Noise[x, y] = perlinValue;
+                _PixelColor[y * _MapWidth + x] = Color.Lerp(Color.black, Color.white, _Noise[x, y]);
+            }
+        }
+
+        _GeneratedTexture.SetPixels(_PixelColor);
+        _GeneratedTexture.Apply();
+        _NoiseMapRenderer.material.mainTexture = _GeneratedTexture;
+    }
+
+    public float[,] GetNoiseData()
+    {
+        return _Noise;
+    }
+
+}
---
+++ Projet Unity/EJS_PROG/Assets/Scripts/Terrain/TerrainGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    private Terrain _Terrain;
+    private TerrainData _Data;
+
+    [SerializeField]
+    private float HeightScale;
+
+    public void Start()
+    {
+        _Terrain = gameObject.AddComponent<Terrain>();
+        _Data = new TerrainData();       
+    }
+
+    public void GenerateTerrain()
+    {
+        _Data.heightmapResolution = NoiseMapGenerator.Get().Width + 1;
+        _Data.size = new Vector3(NoiseMapGenerator.Get().Width, HeightScale, NoiseMapGenerator.Get().Height);
+        _Data.SetHeights(0, 0, NoiseMapGenerator.Get().GetNoiseData());
+
+        _Terrain.terrainData = _Data;
+    }
+}
